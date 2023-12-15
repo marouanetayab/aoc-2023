34536032a62449b9438f8e9a03962d56d774325c
marouanetayab <mtayab1994@gmail.com>
2023-12-15 07:10:08 +0100

    remove global sum variable

--- Day15/solution.js
+++ Day15/solution.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const input = fs.readFileSync('input.txt', 'utf8').trim().split(',');
-let sum = 0;
+const input = fs.readFileSync("input.txt", "utf8").trim().split(",");
 
 function generateHash(str) {
   let value = 0;
@@ -13,39 +12,39 @@ function generateHash(str) {
   return value;
 }
 
-for(let part of ['part1', 'part2']) {
+for (let part of ["part1", "part2"]) {
   const lenses = Array.from({ length: 256 }, () => []);
   const lensLengths = Array.from({ length: 256 }, () => ({}));
-  sum = 0;
+  let sum = 0;
   input.forEach((step) => {
-    if (part === 'part1') sum += generateHash(step);
+    if (part === "part1") sum += generateHash(step);
     else {
-      const label = step.split('=')[0].split('-')[0];
-    const hashValue = generateHash(label);
-  
-    if (step.includes('-')) {
-      const lensIndex = lenses[hashValue].indexOf(label);
-      if (lensIndex !== -1) {
-        lenses[hashValue].splice(lensIndex, 1);
+      const label = step.split("=")[0].split("-")[0];
+      const hashValue = generateHash(label);
+
+      if (step.includes("-")) {
+        const lensIndex = lenses[hashValue].indexOf(label);
+        if (lensIndex !== -1) {
+          lenses[hashValue].splice(lensIndex, 1);
+        }
       }
-    }
-  
-    if (step.includes('=')) {
-      if (!lenses[hashValue].includes(label)) {
-        lenses[hashValue].push(label);
+
+      if (step.includes("=")) {
+        if (!lenses[hashValue].includes(label)) {
+          lenses[hashValue].push(label);
+        }
+        lensLengths[hashValue][label] = parseInt(step.split("=")[1]);
       }
-      lensLengths[hashValue][label] = parseInt(step.split('=')[1]);
-    }
     }
   });
 
-  if(part === 'part2') {
+  if (part === "part2") {
     lenses.forEach((lens, i) => {
       lens.forEach((hashLabel, j) => {
         sum += (i + 1) * (j + 1) * lensLengths[i][hashLabel];
       });
-    });    
+    });
   }
 
-  console.log(part, ': ', sum);
+  console.log(part, ": ", sum);
 }
